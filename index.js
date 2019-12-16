b643726fefd16b8752aa41f80988cbf88fc85ccc
Doug Kintop <dkintop@hotmail.com>
2019-12-16 17:57:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
 // Write your solution in this file!
-function updateDriverWithKeyAndValue(driver, key, value) {
-  const newDriver = driver;
-  debugger;
-  newDriver[key] = value;
-  return newDriver;
-}
