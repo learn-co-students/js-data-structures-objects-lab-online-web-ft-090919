deaa2612ab2c59e326e154554fa677955ee99f0c
clonex10100 <clonex10100@gmail.com>
2020-02-21 06:04:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const driver = { foo: 'bar', ree: 'tard' }
+function updateDriverWithKeyAndValue(driver, key, value) {
+    driver = {...driver};
+    driver[key] = value;
+    return driver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+    driver = {...driver};
+    delete driver[key];
+    return driver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
